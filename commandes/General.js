3c826fba79c7aa7b44d368920ccb78bf6c4b80fa
Charles ke <167456480+Charleskenya1@users.noreply.github.com>
2025-05-10 11:51:34 +0300

    Update General.js

--- commandes/General.js
+++ commandes/General.js
@@ -95,7 +95,7 @@ else {
 zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
-  repondre("THANK YOU FOR CHOOSING CHARLESKE-VMD, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://https://whatsapp.com/channel/0029Vao2hgeChq6HJ5bmlZ3K] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://https://chat.whatsapp.com/EFh0BWKa0Uf92A7RgBwpuu] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://youtube.com/@charles_odhiambo_official?si=n6lKIy2ViQauVS_n] \n\n\n𝑪𝒓𝒆ated by Charles ke") 
+  repondre("THANK YOU FOR CHOOSING CHARLESKE-VMD, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[ https://https://whatsapp.com/channel/0029Vao2hgeChq6HJ5bmlZ3K ] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[ https://https://chat.whatsapp.com/EFh0BWKa0Uf92A7RgBwpuu ] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://youtube.com/@charles_odhiambo_official?si=n6lKIy2ViQauVS_n] \n\n\n𝑪𝒓𝒆ated by Charles ke") 
   await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING CHARLESKE-VMD,MAKE SURE YOU FOLLOW THESE LINKS. `},{quoted :ms})
 
 })
