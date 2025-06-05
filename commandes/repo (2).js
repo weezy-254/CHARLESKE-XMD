b356195cc7275e765fcd71ac57e8374e6e192cbe
Charles ke <167456480+Charleskenya1@users.noreply.github.com>
2025-06-05 14:36:21 +0300

    Update repo (2).js

--- commandes/repo (2).js
+++ commandes/repo (2).js
@@ -71,7 +71,7 @@ commands.forEach((command) => {
         try {
             // Send the follow-up image first with a caption
             await zk.sendMessage(dest, {
-                image: { url: "https://i.imgur.com/y62aAn8.png" }, // Updated image
+                image: { url: "h" }, // Updated image
                 caption: `✨ Repository Highlights ✨\n\n🛠️ Developed by *Charleske  ke*\n📢 Stay updated\nhttps://chat.whatsapp.com/\n\nRepo Url\nhttps://github.com/Charleskenya1/CHARLESKE-XMD`,
             });
 
